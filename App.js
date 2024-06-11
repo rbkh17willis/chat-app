301820b9e3ec3d60e0381c07ee8b0c97e69ae5a2
rbkh17willis <124805813+rbkh17willis@users.noreply.github.com>
2024-06-11 16:05:00 -0700

    Chat bubbles, firstore

--- App.js
+++ App.js
@@ -1,41 +1,66 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, LogBox } from 'react-native';
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
 // import the screens
 import Start from './components/Start';
 import Chat from './components/Chat';
 
+// import Firestore
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { StyleSheet, Text, View } from 'react-native';
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  // Web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAqK2fgT535Xhiu2iMdSA2ef2VvzshALXM",
+    authDomain: "chatapp-be271.firebaseapp.com",
+    projectId: "chatapp-be271",
+    storageBucket: "chatapp-be271.appspot.com",
+    messagingSenderId: "802731673424",
+    appId: "1:802731673424:web:9588c3493fa18633e30e55",
+    measurementId: "G-LGH18MKM8N"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app)
+
   return (
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Start"
       >
         <Stack.Screen
-           name="Start"
-           component={Start}
+          name="Start"
+          component={Start}
         />
         <Stack.Screen
           name="Chat"
-          component={Chat}
-        />
-      </Stack.Navigator>  
-      </NavigationContainer>
+        >
+            {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default App;
--- components/Start.js
+++ components/Start.js
@@ -1,136 +1,146 @@
 import { useState } from 'react';
-import { StyleSheet, View, Text, Button, TextInput, ImageBackground, TouchableOpacity } from 'react-native';
+import { 
+    StyleSheet, 
+    View, 
+    Text, 
+    TouchableOpacity, 
+    TextInput, 
+    ImageBackground, 
+    KeyboardAvoidingView, 
+    Platform,
+    Alert
+} from 'react-native';
+import { getAuth, signInAnonymously } from "firebase/auth";
 
 const Start = ({ navigation }) => {
+    const auth = getAuth();
     const [name, setName] = useState('');
-    const colors = ['#090C08', '#474056', '#8A95A5', '#B9C6AE'];
     const [background, setBackground] = useState('');
- return (
-   <View style={styles.container}>
-     <ImageBackground
-       source={require("../images/bgImage.png")}
-       style={styles.imageBackground}
-     >
-       <Text style={styles.title}>ConnectoChat !</Text>
-       <View style={styles.box}>
-         {/* user types name */}
-         <TextInput
-           style={styles.textInput}
-           value={name}
-           onChangeText={setName}
-           placeholder="Your name"
-         />
-         <Text style={styles.chooseBgColor}>Choose Background Color</Text>
-         {/* user selects backgroung color */}
-         <View style={styles.colorButtonContainer}>
-           {colors.map((color, index) => (
-             <TouchableOpacity
-               key={index}
-               accessible={true}
-               accessibilityRole="button"
-               accessibilityHint="Lets you choose background color for your chat screen"
-               style={[
-                 styles.colorButton,
-                 { backgroundColor: color },
-                 background === color && styles.selectedColor,
-               ]}
-               onPress={() => setBackground(color)}
-             />
-           ))}
-         </View>
-         {/* to start chat */}
-         <TouchableOpacity
-           accessible={true}
-           accessibilityRole="button"
-           accessibilityHint="Lets you choose to enter the chat room"
-           style={styles.button}
-           onPress={() =>
-             navigation.navigate("Chat", { name: name, background: background })
-           }
-         >
-           <Text style={styles.buttonText}>Start Chatting</Text>
-         </TouchableOpacity>
-       </View>
-     </ImageBackground>
-   </View>
- );
+    const colors = ['#090C08', '#474056', '#8A95A5', '#B9C6AE'];
+
+    const signInUser = () => {
+        signInAnonymously(auth)
+          .then(result => {
+            navigation.navigate("Chat", { name: name, background: background, userID: result.user.uid });
+            Alert.alert("Signed in Successfully!");
+          })
+          .catch((error) => {
+            Alert.alert("Unable to sign in, try later again.");
+          })
+    }
+
+    return (
+        <ImageBackground source={require('../images/bgImage.png')} style={styles.bgImage} resizeMode="cover">
+            <Text style={styles.appTitle}>Welcome!</Text>
+            <View style={styles.box}>
+                {/* Input user name \*/}
+                <TextInput
+                    accessibilityLabel="Username input"
+                    accessibilityRole="text"
+                    style={styles.textInput}
+                    value={name}
+                    onChangeText={setName}
+                    placeholder='Type your username here'
+                />
+                <Text style={styles.chooseBackgroundColor}>Choose Background Color</Text>
+                {/* Choose background color of chat \*/}
+                <View style={styles.colorButtonsBox}>
+                    {colors.map((color, index) => (
+                        <TouchableOpacity
+                            accessibilityLabel="Color Button"
+                            accessibilityHint="Lets you choose a backgroundcolor for your chat."
+                            accessibilityRole="button"
+                            key={index}
+                            style={[styles.colorButton, { backgroundColor: color }, background === color && styles.selected]}
+                            onPress={() => setBackground(color)}
+                        />
+                    ))}
+                </View>
+                {/* Start Chat \*/}
+                <TouchableOpacity
+                    accessibilityLabel="Start Chatting"
+                    accessibilityRole="button"
+                    style={styles.button}
+                    onPress={signInUser}>
+                    <Text style={styles.buttonText}>Start Chatting</Text>
+                </TouchableOpacity>
+            </View>
+            {Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" /> : null}
+        </ImageBackground>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  imageBackground: {
-    flex: 1,  
-    alignItems: 'center',
-    justifyContent: 'center', 
-    height: '100%',
-    width: '100%',
-  },
-  title: {
-    flex: 1,
-    fontSize: 45, 
-    fontWeight: '600', 
-    color: '#FFFFFF',
-    margin: 25,
-  },
-  box: {
-    // backgroundColor: '#ffffff', 
-    backgroundColor: '#f2f2f2',
-    borderRadius: 4,
-    width: '88%',
-    height: '50%', 
-    alignItems: 'center',
-    justifyContent: 'space-around', 
-  },
-  textInput: {
-    width: '88%',
-    borderColor: '#757083',
-    borderRadius: 4,
-    color: '#757083',
-    fontSize: 16, 
-    fontWeight: '300', 
-    opacity: 50,
-    padding: 15,
-    borderWidth: 1,
-    marginBottom: 10,
-  },
-  chooseBgColor:{
-    color: '#757083',
-    fontSize: 16,
-    fontWeight: '300',
-    opacity: 100,
-  },
-  colorButtonContainer:{
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  colorButton: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    margin: 5
-},
-selectedColor:{
-    borderColor: '#c0c0c0',
-    borderWidth: 1,
-},
-  button:{
-    alignItems: 'center',
-    backgroundColor: '#757083',
-    borderRadius: 4,
-    height: '20%',
-    justifyContent: 'center',
-    padding: 10,
-    width: '88%',
-  },
-  buttonText: {
-    fontSize: 16,
-    fontWeight: '600',
-    color: '#FFFFFF',
-  }
+    bgImage: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    appTitle: {
+        fontSize: 45,
+        fontWeight: '600',
+        color: '#ffffff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 20
+    },
+    box: {
+        backgroundColor: '#ffffff',
+        width: '88%',
+        height: '44%',
+        alignItems: 'center',
+        marginBottom: 30,
+        justifyContent: 'space-evenly',
+    },
+    textInput: {
+        fontSize: 16,
+        fontWeight: '300',
+        color: '#757083',
+        width: '88%',
+        opacity: 50,
+        padding: 15,
+        borderWidth: 1,
+        marginTop: '8%',
+        marginBottom: 15,
+        top: 5,
+        borderColor: '#757083',
+    },
+    chooseBackgroundColor: {
+        flex: 1,
+        fontSize: 16,
+        fontWeight: '300',
+        color: '#757083',
+        marginTop: 20,
+    },
+    colorButtonsBox: {
+        flexDirection: 'row',
+        width: '80%',
+        justifyContent: "space-between",
+        top: 5,
+        bottom: 5
+    },
+    colorButton: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        margin: 10
+    },
+    selected: {
+        borderColor: 'black',
+        borderWidth: 1
+    },
+    button: {
+        backgroundColor: '#757083',
+        padding: 15,
+        margin: 20,
+        alignItems: 'center',
+        width: '88%'
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#ffffff',
+    }
 });
 
 export default Start;
